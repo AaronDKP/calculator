8586a54c2327229cd0210a1f707162f8c328b5a1
AaronDKP <138345602+AaronDKP@users.noreply.github.com>
2023-09-22 09:28:09 +1000

    Functions for basic calculations
    Add, Subtract, Multiply and Divide now functional for calculations using 2 inputs

--- javascript.js
+++ javascript.js
@@ -2,55 +2,61 @@ let input1 = '';
 let input2 = '';
 let operator = '';
 
-
-const totalInput = function() {
-    const numbers = document.getElementsByClassName('number');
-    const operators = document.getElementsByClassName('operator');
+const numbers = document.getElementsByClassName('number');
+const operators = document.getElementsByClassName('operator');
+const calcDisplay = document.querySelector('#calcDisplay');
 
     for (const num of numbers) {
         num.addEventListener('click', () => {
-            operator !== '' ? input2 += num.textContent : input1 += num.textContent;
-            console.log(input1 + '' + operator + '' + input2);
+            operator == '' ? input1 += num.textContent : input2 += num.textContent;
+            calcDisplay.textContent = input1 + ' ' + operator + ' ' + input2;
         });
     }
 
     for (const op of operators) {
         op.addEventListener('click', () => {
-            operator = op.textContent;
+            if (input1 !== '') {operator = op.textContent};
+            calcDisplay.textContent = input1 + ' ' + operator + ' ' + input2;
         });
     }
 
-}
+const c = document.querySelector('#C');
+    c.addEventListener('click', () => {
+        input1 = '';
+        input2 = '';
+        operator = '';
+        calcDisplay.textContent = input1 + ' ' + operator + ' ' + input2;
+    });
+
+const equals = document.querySelector('#equals');
+    equals.addEventListener('click', () => {
+        calcDisplay.textContent = `${operate(input1, operator, input2)}`;
+    })
 
 
-console.log(totalInput());
- 
 
 const operate = function(input1, operator, input2) {
     switch(operator){
-        case '+': add(input1, input2);
-            break;
-        case '-': subtract(input1, input2);
-            break;
-        case '*': multiply(input1, input2);
-            break;
-        case '/': divide(input1, input2);
+        case '+': return add(input1, input2);
+        case '-': return subtract(input1, input2);
+        case '*': return multiply(input1, input2);
+        case '/': return divide(input1, input2);
     }
 }
 
 const add = function(input1, input2) {
-	return (input1 += input2)
+	return parseFloat(input1) + parseFloat(input2);
 };
 
 const subtract = function(input1, input2) {
-	return (input1 -= input2)
+	return input1 -= input2;
 };
 
 const multiply = function(input1, input2) {
-    return (input1 *= input2);
+    return input1 *= input2;
   };
 
 const divide = function(input1, input2) {
-    return (input1 /= input2);
+    return input1 /= input2;
 }
 
